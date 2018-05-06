1b1e3410957df653da96f08aec1211714d528767
Karalic Slavisa <slavisa.karalic@gmail.com>
2018-05-06 09:46:30 +0200

    Tut3 Adding Login Validation with dummy data

--- flaskblog.py
+++ flaskblog.py
@@ -45,9 +45,16 @@ def register():
                             title='Register',
                             form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You habe been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+        return redirect(url_for('home'))
     return render_template("login.html",
                             title='Login',
                             form=form)
