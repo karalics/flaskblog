ebbfda95d64f6e3630701b48839dbc11fdfae8d2
Karalic Slavisa <slavisa.karalic@gmail.com>
2018-05-06 08:55:56 +0200

    Tut3 Adding Registration Form

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from flask import render_template
 from flask import url_for
+from forms import RegistrationForm, LoginForm
+
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'ee299c6e45dff93b72946c1f4f501de2'
 
 posts = [
         {
@@ -25,7 +30,22 @@ def hello():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title='About')
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template("register.html",
+                            title='Register',
+                            form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html",
+                            title='Login',
+                            form=form)
 
 
 if __name__ == '__main__':
