04cee024251892900922ce9cb4320ee2fca0e149
Karalic Slavisa <slavisa.karalic@gmail.com>
2018-05-09 20:54:04 +0200

    Tut6 adding gitignore fixing validator

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
@@ -22,7 +22,7 @@ class RegistrationForm(FlaskForm):
         if user:
             raise ValidationError('That username is taken. Please choose a different one.')
 
-    def validate_username(self, email):
+    def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('That email is taken. Please choose a different one.')
