c8b84a5e91a5def9ef0cdec4f59a374c4f0d556f
Karalic Slavisa <slavisa.karalic@gmail.com>
2018-05-09 21:07:13 +0200

    Tut6 adding working login

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -8,6 +9,7 @@ app.config['SECRET_KEY'] = 'ee299c6e45dff93b72946c1f4f501de2'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 
 from flaskblog import routes
