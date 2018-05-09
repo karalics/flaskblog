e43ce8aab497c47415f080c3d6ad374bd270b25a
Karalic Slavisa <slavisa.karalic@gmail.com>
2018-05-09 23:54:18 +0200

    Tut8 Writing and listing Database Posts

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -8,24 +8,11 @@ from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
 
-posts = [
-        {
-            'author': 'Slavisa Karalic',
-            'title': 'Blog Post 1',
-            'content': 'First post content',
-            'date_posted': 'April 20, 2018'
-            },
-        {
-            'author': 'Slavisa Karalic',
-            'title': 'Blog Post 2',
-            'content': 'Second post content',
-            'date_posted': 'April 21, 2018'
-            }
-        ]
 
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template("home.html", posts=posts)
 
 
@@ -114,6 +101,9 @@ def account():
 def new_post():
     form = PostForm()
     if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
         flash('Your Post has been created!', 'success')
         return redirect(url_for('home'))
     return render_template('create_post.html', title='New Post', form=form)
