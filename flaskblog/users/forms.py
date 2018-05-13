803c2d8c4a113fdb8ed9da508a735ae460447c48
Karalic Slavisa <slavisa.karalic@gmail.com>
2018-05-13 09:54:28 +0200

    Tut11 Refactoring: moving forms classes to packages small fix

--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -63,3 +63,16 @@ class ResetPasswordForm(FlaskForm):
                               validators=[DataRequired(),
                               EqualTo('password')])
     submit = SubmitField('Reset Password')
+
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+                         validators=[DataRequired(),
+                         Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first.')
+
